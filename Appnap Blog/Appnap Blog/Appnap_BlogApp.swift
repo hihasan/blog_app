97ce153089473c2c9463a6d9cd3531f14686a0fa
Hasan Al Mamun <hihasan07@gmail.com>
2023-05-29 14:56:38 +0600

    initila commit

---
+++ Appnap Blog/Appnap Blog/Appnap_BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appnap_BlogApp.swift
+//  Appnap Blog
+//
+//  Created by Appnap ws27 on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Appnap_BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
